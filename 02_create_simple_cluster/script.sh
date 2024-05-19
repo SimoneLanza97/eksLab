b5094fea0bb5f922e626e78e226171d81d9ac63a
Lanza <lanzarottosimone@gmail.com>
2024-05-19 16:05:05 +0200

    ebs storage tutorial

--- 02_create_simple_cluster/script.sh
+++ 02_create_simple_cluster/script.sh
@@ -17,6 +17,12 @@ if [ ${operation} == "01" ]; then
 
     eksctl create nodegroup --cluster=BerryCluster01 --region=eu-west-1 --name=BerryCluster01-nodeGroup01 --node-type=t3.medium --nodes=2 --nodes-min=2 --nodes-max=4 --node-volume-size=20 --ssh-access --ssh-public-key=BerryKey --managed --asg-access --external-dns-access --full-ecr-access --appmesh-access --alb-ingress-access  || exit 
 
+    rolearn= $(kubectl -n kube-system get configmap aws-auth -o json | jq -r '.data.mapRoles | split("\n") | map(select(startswith("  rolearn: "))) | map(split(": ")[1]) | .[]')
+
+    aws iam attach-role-policy --role-name ${rolearn} --policy-arn arn:aws:iam::637364600367:policy/EBS-Storage-Policy-Complete
+
+    kubectl apply -k "github.com/kubernetes-sigs/aws-ebs-csi-driver/deploy/kubernetes/overlays/stable/?ref=master"
+
 elif [ ${operation} == "02" ]; then 
 
     eksctl delete cluster --name BerryCluster01 || echo "operation failed! remove the cluster by using the aws console" && exit 
@@ -31,7 +37,7 @@ echo -e "Would you like to deploy the worpress deployment for testing?\n yes/NO"
 
 read operation2
 
-if [ ${operation2} == "yes" ]; then 
+if [ ${operation2} -eq "yes" ]; then 
 
     kubectl apply -f ${manifest_path}/deployment.yaml || exit
     kubectl apply -f ${manifest_path}/service.yaml || exit
